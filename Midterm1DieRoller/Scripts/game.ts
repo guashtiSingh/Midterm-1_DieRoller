3c3ce60c2e1c745cbdb107650b92774c7979a063
guashtiSingh <ashtisingh@hotmail.com>
2015-06-16 10:45:14 -0400

    Update

--- Midterm1DieRoller/Scripts/game.ts
+++ Midterm1DieRoller/Scripts/game.ts
@@ -25,6 +25,12 @@ var manifest = [
 // Game Variables
 var dieLabel: createjs.Text;
 var rollButton: createjs.Bitmap;
+var one: createjs.Bitmap;
+var two: createjs.Bitmap;
+var three: createjs.Bitmap;
+var four: createjs.Bitmap;
+var five: createjs.Bitmap;
+var six: createjs.Bitmap;
 
 var rollResult;
 var die = "";
@@ -90,68 +96,83 @@ function checkRange(value, lowerBounds, upperBounds) {
 }
 
 function roll() {
-
-    var dieLine = [" ", " "];
-    var outCome = [0,0];
+    var outCome = [0, 0];
     for (var roll = 0; roll < 6; roll++) {
         outCome[roll] = Math.floor((Math.random() * 6) + 1);
-
-        switch (outCome[roll]) {
-            case checkRange(outCome[roll], 1, 27):
-                dieLine[roll] = "one";
-                break;
-            case checkRange(outCome[roll], 28, 27):
-                dieLine[roll] = "two";
-                break;
-            case checkRange(outCome[roll], 38, 46):
-                dieLine[roll] = "three";
-                break;
-            case checkRange(outCome[roll], 47, 54):
-                dieLine[roll] = "four";
-                break;
-            case checkRange(outCome[roll], 55, 59):
-                dieLine[roll] = "five";
-                break;
-            case checkRange(outCome[roll], 60, 62):
-                dieLine[roll] = "six";
-                break;
-        }
     }
-    return dieLine;
 }
 
-// Callback function that allows me to respond to button click events
-function rollButtonClicked(event: createjs.MouseEvent) {
-    createjs.Sound.play("clicked");
+    // Callback function that allows me to respond to button click events
+    function rollButtonClicked(event: createjs.MouseEvent) {
+        createjs.Sound.play("clicked");
 
-    rollResult = roll();
-    die = rollResult[0] + " - " + rollResult[1] ;
+        rollResult = roll();
+        die = rollResult[0] + " - " + rollResult[1];
+        console.log(die);
+    }
 
-    console.log(die);
-}
+    // Mouseover event
+    function RollButtonOver() {
+        rollButton.alpha = 0.8;
+    }
 
-// Mouseover event
-function RollButtonOver() {
-    rollButton.alpha = 0.8;
-}
+    // Mouseout event
+    function RollButtonOut() {
+        rollButton.alpha = 1.0;
+    }
 
-// Mouseout event
-function RollButtonOut() {
-    rollButton.alpha = 1.0;
-}
+    // Our Main Game Function
+    function main() {
+        console.log("Game is Running");
 
-// Our Main Game Function
-function main() {
-    console.log("Game is Running");
+        one = new createjs.Bitmap(assets.getResult("one"));
+        one.regX = one.getBounds().width * 0.5;
+        one.regY = one.getBounds().height * 0.5;
+        one.x = 100;
+        one.y = 100;
+        stage.addChild(one);
 
-    rollButton = new createjs.Bitmap(assets.getResult("rollButton"));
-    rollButton.regX = rollButton.getBounds().width * 0.5;
-    rollButton.regY = rollButton.getBounds().height * 0.5;
-    rollButton.x = 100;
-    rollButton.y = 170;
-    stage.addChild(rollButton);
-    rollButton.on("click", pinkButtonClicked);
-    rollButton.on("mouseover", RollButtonOver);
-    rollButton.on("mouseout", RollButtonOut);
-    rollButton.on("click", rollButtonClicked, this);
+        two = new createjs.Bitmap(assets.getResult("two"));
+        two.regX = two.getBounds().width * 0.5;
+        two.regY = two.getBounds().height * 0.5;
+        two.x = 100;
+        two.y = 100;
+        stage.addChild(two);
+
+        three = new createjs.Bitmap(assets.getResult("three"));
+        three.regX = three.getBounds().width * 0.5;
+        three.regY = three.getBounds().height * 0.5;
+        three.x = 100;
+        three.y = 100;
+        stage.addChild(three);
+
+        four = new createjs.Bitmap(assets.getResult("four"));
+        four.regX = four.getBounds().width * 0.5;
+        four.regY = four.getBounds().height * 0.5;
+        four.x = 100;
+        four.y = 100;
+        stage.addChild(four);
+
+        five = new createjs.Bitmap(assets.getResult("five"));
+        five.regX = five.getBounds().width * 0.5;
+        five.regY = five.getBounds().height * 0.5;
+        five.x = 100;
+        five.y = 100;
+        stage.addChild(five);
+
+        six = new createjs.Bitmap(assets.getResult("six"));
+        six.regX = one.getBounds().width * 0.5;
+        six.regY = one.getBounds().height * 0.5;
+        six.x = 100;
+        six.y = 100;
+        stage.addChild(six);
+
+        rollButton = new createjs.Bitmap(assets.getResult("rollButton"));
+        rollButton.x = 200;
+        rollButton.y = 100;
+        stage.addChild(rollButton);
+        rollButton.on("click", pinkButtonClicked);
+        rollButton.on("mouseover", RollButtonOver);
+        rollButton.on("mouseout", RollButtonOut);
+        rollButton.on("click", rollButtonClicked, this);
 }
